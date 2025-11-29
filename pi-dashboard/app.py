1a2b77bbaaaae3661c023ae85d5d5f37e512d606
NikiDev007 <niklasemilschwartz@gmail.com>
2025-11-29 13:59:22 +0100

    added favicon, changed linking from html to flask

--- pi-dashboard/app.py
+++ pi-dashboard/app.py
@@ -58,6 +58,11 @@ def status_api():
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/dashboard')
+def dashboard():
     return render_template('dashboard.html')
 
 
